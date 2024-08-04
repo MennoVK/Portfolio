dc2b01b4efc2c0fbf57a298bfa1e195153973bb4
Menno Veerkamp <veerkampmenno@gmail.com>
2024-08-04 20:23:43 +0200

    create project

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main className="flex flex-col items-center justify-center min-h-screen p-24">
+            <h1>Coming Soon</h1>
+        </main>
+    )
+}
