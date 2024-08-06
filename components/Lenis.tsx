4e5783517b1933d638db030e8820bd9997d1bdd3
Menno Veerkamp <veerkampmenno@gmail.com>
2024-08-06 12:21:00 +0200

    feat(scrolling): add lenis smooth scroll

---
+++ components/Lenis.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { ReactLenis } from "@studio-freight/react-lenis";
+import { ReactNode } from "react";
+
+
+interface LenisProps {
+    children?: ReactNode
+}
+
+export const Lenis = ({ children }: LenisProps) => {
+    return (
+        <ReactLenis root options={{ lerp: 0.1, duration: 0.5 }}>
+            {children}
+        </ReactLenis>
+    );
+};
