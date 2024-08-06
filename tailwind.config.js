948b51201f056698cf6d5cb61e06405bd7c83e0f
Menno Veerkamp <veerkampmenno@gmail.com>
2024-08-06 12:20:06 +0200

    feat(tailwind): add custom config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,44 @@
+const spacing = new Array(450).fill(null).reduce((result, item, i) => {
+    result[i] = i < 4 ? `${i}px` : `${i / 16}rem`
+    return result
+}, {})
+
+const SIZES = [
+    12, 20, 30, 36, 50, 65, 66
+];
+
+const max = (px) => {
+    return `max(${px}px,${px / 16}rem)`;
+};
+
+const fontSize = {};
+SIZES.forEach((size) => {
+    fontSize[size] = [max(size)];
+});
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./blocks/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        colors: {
+            white: "#ffffff",
+            "tundora-grey": "#4B4A4A",
+            black: "#121212",
+            red: "#ff0000",
+        },
+        borderRadius: {
+            lg: "30px",
+        },
+        spacing,
+        fontSize,
+        extend: {
+            
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
