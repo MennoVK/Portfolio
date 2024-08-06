6ba157d6ef9135aa2975012d5981f6fd22d69a41
Menno Veerkamp <veerkampmenno@gmail.com>
2024-08-06 12:22:47 +0200

    feat(layout): setup site structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 
+import { Footer } from "@/components/Footer"
+import { Header } from "@/components/Header"
+import { Lenis } from "@/components/Lenis"
+
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -17,8 +21,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
     return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
-        </html>
+        <Lenis>
+            <html lang="en">
+                <body className={inter.className}>
+                    <main>
+                        <Header />
+                        {children}
+                        <Footer />
+                    </main>
+                </body>
+            </html>
+        </Lenis>
     )
 }
