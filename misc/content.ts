78cc810eb104733f4056461128dfe71a1456a5c1
Menno Veerkamp <veerkampmenno@gmail.com>
2024-08-06 12:20:49 +0200

    feat(content): add site content

---
+++ misc/content.ts
@@ -0,0 +1,43 @@
+export const projects = [
+    {
+        title: "arthur bruyere portfolio", 
+        description: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page", 
+        aspects: "dev", 
+        year: 2024, 
+        images: [
+            {file: "square1.png", alt: "testalt"},
+            {file: "rectangle1.png", alt: "testalt"},
+            {file: "square1.png", alt: "testalt"},
+            {file: "square1.png", alt: "testalt"},
+        ]
+    },
+    {
+        title: "Project", 
+        aspects: "dev",
+        year: 2024, 
+        images: [
+        ]
+    },
+    {
+        title: "test",
+    },
+]
+
+export const bio = "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page"
+
+export const skills = [
+    "html",
+    "CSS/Sass",
+    "Javascript",
+    "React",
+    "Nextjs",
+    "Vue",
+    "Laravel",
+    "Git",
+    "TailwindCSS",
+]
+
+export const experience = [
+    {work: "student assistent - Hogeschool Leiden", year: "2022-23"},
+    {work: "Internship frontend development - Numbered studio", year: "2023-24"},
+]
