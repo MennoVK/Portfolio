7016895480e7bf9a1a913f7b06ab995dc5ad9a79
Menno Veerkamp <veerkampmenno@gmail.com>
2024-08-10 14:30:43 +0200

    feat(styling): fix width of left and right parts

--- blocks/ImageProjectsDiptych.tsx
+++ blocks/ImageProjectsDiptych.tsx
@@ -8,15 +8,15 @@ import { projects } from "@/misc/content"
 export const ImageProjectsDiptych = () => {
     return (
         <section className="flex w-full h-screen gap-10 px-20 py-32">
-            <div className="flex-grow h-full border rounded-tl-lg rounded-br-lg border-tundora-grey basis-0 p-95">
+            <div className="w-3/5 h-full border rounded-tl-lg rounded-br-lg border-tundora-grey p-95">
                 <Image
                     alt="placeholder"
                     src="/circle.gif"
                     width={610}
                     height={610}
-                    className="w-full "/>
+                    className="w-full max-w-[1000px]"/>
             </div>
-            <div className="flex flex-col flex-grow gap-40 basis-0">
+            <div className="flex flex-col w-2/5 gap-40">
                 <h2 className="uppercase text-12 text-white/50">Projects</h2>
                 <p className="text-30">Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page</p>
                 <div className="w-full uppercase">
