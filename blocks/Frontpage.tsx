85ae44becf4dbad7d245c20046ded3ba2d035cc7
Menno Veerkamp <veerkampmenno@gmail.com>
2024-09-10 12:04:18 +0200

    feat(frontpage): responsiveness

--- blocks/Frontpage.tsx
+++ blocks/Frontpage.tsx
@@ -4,9 +4,9 @@ import Image from "next/image"
 export const Frontpage = () => {
     return (
         <section className="w-full h-screen">
-            <div className="absolute h-full p-20 uppercase">
-                <h1 className="relative w-3/4 -translate-y-1/2 text-50 top-1/2">Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page</h1>
-                <p className="absolute bottom-20 text-12">
+            <div className="absolute h-full p-10 sm:p-20">
+                <h1 className="relative -translate-y-1/2 sm:w-3/4 text-28 sm:text-50 top-1/2">Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page</h1>
+                <p className="absolute uppercase bottom-20 text-12">
                     Scroll down
                     <Image
                         src="/svgs/arrow-down.svg"
