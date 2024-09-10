816d8fba6b8bc2712ab9ad50548840d2f47a8d95
Menno Veerkamp <veerkampmenno@gmail.com>
2024-09-10 11:53:31 +0200

    feat(misc): change name and add currentYear

--- misc/time.ts
+++ misc/time.ts
@@ -1,11 +1,12 @@
 const birthDate = new Date(2004, 2, 2);
 const today = new Date();
 
-let age = today.getFullYear() - birthDate.getFullYear();
+let currentYear = today.getFullYear();
+let age = currentYear - birthDate.getFullYear();
 
 const monthDiff = today.getMonth() - birthDate.getMonth();
 if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
     age--;
 }
 
-export default age;
+export {age, currentYear};
