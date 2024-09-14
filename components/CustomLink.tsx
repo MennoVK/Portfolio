3f0dd33bed666a01c5b13c45a137c1cde3beb684
Menno Veerkamp <veerkampmenno@gmail.com>
2024-09-14 13:49:43 +0200

    fix(link): undo wrapping test

--- components/CustomLink.tsx
+++ components/CustomLink.tsx
@@ -7,7 +7,7 @@ import { tv } from "tailwind-variants";
 export const linkStyles = tv({
     variants: {
         underline: {
-            true: "after:scale-x-100 relative inline-block w-max after:transition-transform after:absolute after:w-full after:h-1 after:bg-current after:bottom-0 after:left-0 after:hover:scale-x-100 sm:after:scale-x-0 after:origin-bottom-right after:hover:origin-bottom-left",
+            true: "after:scale-x-100 relative inline-block w-fit after:transition-transform after:absolute after:w-full after:h-1 after:bg-current after:bottom-0 after:left-0 after:hover:scale-x-100 sm:after:scale-x-0 after:origin-bottom-right after:hover:origin-bottom-left",
         },
         active: {
             true: "sm:after:scale-x-100 sm:after:hover:scale-x-0 after:origin-bottom-left after:hover:origin-bottom-right" ,
