458f76c4a25939b934e742cad94f5267d336d054
Menno Veerkamp <veerkampmenno@gmail.com>
2024-09-21 12:43:38 +0200

    feat(z-index): fix z-index issue

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Image from "next/image";
 
 import { CustomLink } from "@/components/CustomLink";
 import { error } from "@/misc/content";
@@ -7,21 +7,23 @@ import { error } from "@/misc/content";
 export default function NotFound() {
     return (
         <>
-            <div className="absolute flex flex-col items-center justify-center w-full h-full gap-12 uppercase">
-                <h1 className="text-60 sm:text-90">{error.title}</h1>
-                <CustomLink
-                    href="/"
-                    underline
-                    active
-                    className="text-12">{error.link}</CustomLink>
+            <div className="w-full h-svh">
+                <div className="absolute z-10 flex flex-col items-center justify-center w-full h-full gap-12 uppercase">
+                    <h1 className="text-60 sm:text-90">{error.title}</h1>
+                    <CustomLink
+                        href="/"
+                        underline
+                        active
+                        className="text-12">{error.link}</CustomLink>
+                </div>
+                <Image
+                    src="/shader.gif"
+                    alt="home"
+                    width={1440}
+                    height={900}
+                    className="object-cover w-full h-svh"
+                    unoptimized />
             </div>
-            <Image
-                src="/shader.gif"
-                alt="home"
-                width={1440}
-                height={900}
-                className="object-cover w-full h-[100svh]"
-                unoptimized />
         </>
     );
 }
