16507682cc1a7b131cc0489f6334332fa154fd46
Menno Veerkamp <veerkampmenno@gmail.com>
2024-09-26 14:45:17 +0200

    feat(assets): update content

--- misc/content.ts
+++ misc/content.ts
@@ -1,37 +1,7 @@
 import {age} from "./time"
 
 
-export const projectsTitle = "To practise my skills I've done some personal and school related projects. Below are the most important ones."
-
-export const projects = [
-    {
-        title: "Todo app", 
-        description: "For a school project I chose to learn VueJS. To prove my knowledge I built this Todo list app and hosted it with firebase.", 
-        aspects: "dev",
-        year: 2023, 
-        images: [
-            {file: "1project1.png", alt: "testalt"},
-            {file: "1project2.png", alt: "testalt"},
-        ],
-        link: "https://todoapp-b51e4.web.app/"
-    },
-    {
-        title: "Arthur Bruyere portfolio", 
-        description: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page", 
-        aspects: "dev", 
-        year: 2024, 
-        images: [
-            {file: "2project1.png", alt: "testalt"},
-            {file: "2project2.png", alt: "testalt"},
-            {file: "2project1.png", alt: "testalt"},
-            {file: "2project1.png", alt: "testalt"},
-        ],
-        link: "https://arthur-bruyere.com",
-    },
-    
-]
-
-export const bio = `Hi! I'm Menno Veerkamp. A ${age} year old frontend developer from The Netherlands.`
+export const bio = `Hi! I'm Menno Veerkamp. \n A ${age} year old frontend developer from The Netherlands.`
 
 export const skills = [
     "html",
@@ -46,8 +16,8 @@ export const skills = [
 ]
 
 export const experience = [
-    {work: "student assistent - Hogeschool Leiden", year: "2022-23", description: "As a student assistent my job was to assist teachers in helping first year students with their work. I was also available to share my personal school experiences and helped setting up learning environments."},
     {work: "Internship frontend development - Numbered studio", year: "2023-24", description: "During my internship at Numbered I learned to work in a team on projects for big clients. This included working with modern web development techniques and focussing on performance, accessibility and SEO."},
+    {work: "student assistent - Hogeschool Leiden", year: "2022-23", description: "As a student assistent my job was to assist teachers in helping first year students with their work. I was also available to share my personal school experiences and helped setting up learning environments."},
 ]
 
 export const socials = [
@@ -56,5 +26,49 @@ export const socials = [
 ]
 
 export const email = "Contact@mennoveerkamp.com"
+
+export const projectsTitle = "To practise my skills I've done some personal and school related projects. Below are the most important ones."
+
+export const projects = [
+    // {
+    //     title: "Arthur Bruyere portfolio", 
+    //     description: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page", 
+    //     aspects: "dev", 
+    //     year: 2024, 
+    //     images: [
+    //         {file: "test.png", alt: "testalt"},
+    //         {file: "2project2.png", alt: "testalt"},
+    //         {file: "2project1.png", alt: "testalt"},
+    //         {file: "2project1.png", alt: "testalt"},
+    //     ],
+    //     link: "https://arthur-bruyere.com",
+    // },
+    {
+        title: "Todo app", 
+        description: "For a school project I chose to learn VueJS. To prove my knowledge I built this Todo list app and hosted it with firebase.", 
+        aspects: "dev",
+        year: 2023, 
+        images: [
+            {file: "todo-mobile.png", alt: "testalt"},
+            {file: "todo-desktop-2.png", alt: "testalt"},
+            {file: "todo-mobile-2.png", alt: "testalt"},
+            {file: "todo-mobile-3.png", alt: "testalt"},
+        ],
+        link: "https://todoapp-b51e4.web.app/"
+    },
+    {
+        title: "Stranger things practise website", 
+        description: "For a school project i built a website in Stranger things theme with HTML, CSS and Javascript.", 
+        aspects: "dev",
+        year: 2022, 
+        images: [
+            {file: "stranger-mobile.png", alt: "testalt"},
+            {file: "stranger-desktop.png", alt: "testalt"},
+            {file: "stranger-mobile-2.png", alt: "testalt"},
+            {file: "stranger-mobile-3.png", alt: "testalt"},
+        ],
+        link: "https://eindwebsite-iwder.web.app/"
+    },
+]
 
 export const error = {title: "404 error", link: "Return to home"}
