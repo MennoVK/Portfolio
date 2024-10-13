21d44cb67716cbc69099f42245fa8a4ab53bdefb
Menno Veerkamp <veerkampmenno@gmail.com>
2024-10-13 15:19:08 +0200

    fix(anchor links): fix jump scroll by global store

--- blocks/Hero.tsx
+++ blocks/Hero.tsx
@@ -3,13 +3,16 @@
 import Image from "next/image"
 import { useEffect, useRef } from "react"
 
+import { CustomLink } from "@/components/CustomLink"
 import { Video } from "@/components/Video"
 import { bio } from "@/misc/content"
+import { useLenisStore } from "@/misc/stores/store";
 
 
 export const Hero = () => {
     const videoRef = useRef<HTMLVideoElement | null>(null);
-
+    const lenis = useLenisStore((state) => state.lenis);
+    
     useEffect(() => {
         const handleScroll = () => {
             let offset = window.scrollY;
@@ -29,9 +32,11 @@ export const Hero = () => {
         <section className="w-full overflow-hidden h-svh">
             <div className="absolute w-full h-full p-10 sm:w-3/4 lg:max-w-[1400px] sm:p-20 z-[5] motion-reduce:pointer-events-none">
                 <h1 className="relative -translate-y-1/2 pointer-events-auto text-28 sm:text-50 top-1/2">{bio}</h1>
-                <p
+                <CustomLink
+                    href="#projects"
+                    onClick={() => lenis?.scrollTo("#projects")}
                     className="absolute uppercase pointer-events-auto bottom-20 text-12 lg:text-14 motion-reduce:bottom-100">
-                    Projects
+                    Scroll down
                     <Image
                         src="/svgs/arrow-right.svg"
                         alt="Scroll down arrow"
@@ -39,7 +44,7 @@ export const Hero = () => {
                         height={15}
                         priority
                         className="inline mx-10 animate-custom-bounce motion-reduce:animate-none motion-reduce:rotate-90"/>
-                </p>
+                </CustomLink>
             </div>
             <Video
                 ref={videoRef}
