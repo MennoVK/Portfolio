707ab9c1128008b90e25732d9b6f70ff0fc96537
Menno Veerkamp <veerkampmenno@gmail.com>
2024-12-30 20:19:58 +0100

    feat(date): date update

--- blocks/ImageProjectsDiptych.tsx
+++ blocks/ImageProjectsDiptych.tsx
@@ -22,9 +22,9 @@ export const ImageProjectsDiptych = () => {
 
     return (
         <section
-            className="relative flex w-full h-full gap-10 sm:py-32 sm:h-screen sm:px-20 sm:my-0 my-120"
+            className="sm:py-32 sm:h-screen sm:px-20 sm:my-0 my-120 relative flex w-full h-full gap-10"
             id="projects">
-            <div className="relative items-center justify-center w-full rounded-tl-lg rounded-br-lg sm:flex sm:h-full sm:w-3/5 sm:border border-tundora-grey sm:p-25 lg:p-95">
+            <div className="sm:flex sm:h-full sm:w-3/5 sm:border border-tundora-grey sm:p-25 lg:p-95 relative items-center justify-center w-full rounded-tl-lg rounded-br-lg">
                 {isClient && !isMobile() &&
                     <Image
                         src={`/projects/${projects[activeProject ? activeProject : 0].images[0].file}`}
@@ -46,15 +46,15 @@ export const ImageProjectsDiptych = () => {
                     preload="auto" 
                     poster="/videos/bubbles-thumbnail.webp" />
             </div>
-            <div className="absolute flex flex-col w-full gap-40 px-10 sm:px-0 sm:w-2/5 sm:relative ">
-                <h2 className="uppercase text-12 lg:text-14 text-white/50">Projects</h2>
+            <div className="sm:px-0 sm:w-2/5 sm:relative  absolute flex flex-col w-full gap-40 px-10">
+                <h2 className="text-12 lg:text-14 text-white/50 uppercase">Projects</h2>
                 <p className="text-20 sm:text-30 lg:text-36">{projectsTitle}</p>
                 <div className="w-full uppercase">
                     <div
                         className={clsx("flex justify-between w-full border-t border-tundora-grey text-white/50")}
                     >
-                        <p className="w-1/2 py-20 text-12 lg:text-14">Coming soon</p>
-                        <p className="py-20 text-right text-12 lg:text-14">2024</p>
+                        <p className="text-12 lg:text-14 w-1/2 py-20">Coming soon</p>
+                        <p className="text-12 lg:text-14 py-20 text-right">2025</p>
                     </div>
                     {projects.map((project, idx) => (
                         <CustomLink
@@ -64,9 +64,9 @@ export const ImageProjectsDiptych = () => {
                             onMouseEnter={() => setActiveProject(idx)}
                             onMouseLeave={() => setActiveProject(null)}
                         >
-                            <p className="w-1/2 py-20 text-12 lg:text-14">{project.title}</p>
-                            <p className="py-20 text-center text-12 lg:text-14">{project.aspects}</p>
-                            <p className="py-20 text-right text-12 lg:text-14">{project.year}</p>
+                            <p className="text-12 lg:text-14 w-1/2 py-20">{project.title}</p>
+                            <p className="text-12 lg:text-14 py-20 text-center">{project.aspects}</p>
+                            <p className="text-12 lg:text-14 py-20 text-right">{project.year}</p>
                         </CustomLink>
                     ))}
                     
