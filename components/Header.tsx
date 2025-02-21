a3ad7c71f1c33991cf8ef76129d6c9fcbe99e087
Menno Veerkamp <veerkampmenno@gmail.com>
2025-02-21 15:24:53 +0100

    fix(header): remove fragments

--- components/Header.tsx
+++ components/Header.tsx
@@ -51,36 +51,33 @@ export const Header = () => {
     })
 
     return (
-        <>
-            <header
-                className={clsx(!isVisible && "mdlg:pointer-events-none", sideBarOpen && "sm:overflow-x-hidden sm:overflow-y-scroll", "header fixed z-20 flex justify-between w-full gap-10 p-10 sm:p-20 ")}>
-                <CustomLink
-                    className={clsx(button({ rounded: "tl" }), "w-10/12 mdlg:w-auto flex items-center justify-center text-20 mdlg:text-25 lg:text-30 [transition:backdrop-filter_0.2s,transform_1s]", !isVisible && "mdlg:-translate-y-[150%]", !sideBarOpen && !isVisible && "-translate-y-[150%]" )}
-                    href="/"
-                    onClick={() => setSideBarOpen(false)}>Menno Veerkamp</CustomLink>
-                <button
-                    aria-label="open about me"
-                    onClick={toggleSideBar}
-                    className={clsx(button({rounded: "br"}), "uppercase [transition:backdrop-filter_0.2s,transform_1s_0.15s] motion-reduce:delay-0 w-1/6 mdlg:w-auto", !isVisible && "mdlg:-translate-y-[150%]", !sideBarOpen && !isVisible && "-translate-y-[150%]")}>
-                    <span className="hidden mdlg:inline-block">About me</span>
-                    <span
-                        className={clsx(sideBarOpen && "after:!rotate-0 before:!rotate-180", "inline-block align-baseline relative w-10 h-10 before:content-[''] before:block before:bg-grey before:absolute before:top-2/4 before:left-0 before:w-full before:h-1 after:content-[''] after:block after:bg-grey after:absolute after:top-2/4 after:left-0 after:duration-700 before:duration-700 motion-reduce:after:duration-0 motion-reduce:before:duration-0 after:w-full after:h-1 before:-translate-y-1/2 after:-translate-y-1/2 after:rotate-90 p-5 mx-10")}
-                    />
-                </button>
-                <CustomLink
-                    className={clsx(button({ rounded: "tlbr" }), "items-center hidden mdlg:flex justify-center [transition:backdrop-filter_0.2s,transform_1s_0.3s] motion-reduce:delay-0 uppercase", !isVisible && "-translate-y-[150%]")}
-                    href={`mailto:${email}`}
-                    target="_blank">
-                    {email}
-                    <Image
-                        src="/svgs/arrow-right.svg"
-                        alt="arrow right"
-                        width={18}
-                        height={11}
-                        className="inline ml-5 align-baseline"/>
-                </CustomLink>
-            </header>
-        </>
-
+        <header
+            className={clsx(!isVisible && "mdlg:pointer-events-none", sideBarOpen && "sm:overflow-x-hidden sm:overflow-y-scroll", "header fixed z-20 flex justify-between w-full gap-10 p-10 sm:p-20 ")}>
+            <CustomLink
+                className={clsx(button({ rounded: "tl" }), "w-10/12 mdlg:w-auto flex items-center justify-center text-20 mdlg:text-25 lg:text-30 [transition:backdrop-filter_0.2s,transform_1s]", !isVisible && "mdlg:-translate-y-[150%]", !sideBarOpen && !isVisible && "-translate-y-[150%]" )}
+                href="/"
+                onClick={() => setSideBarOpen(false)}>Menno Veerkamp</CustomLink>
+            <button
+                aria-label="open about me"
+                onClick={toggleSideBar}
+                className={clsx(button({rounded: "br"}), "uppercase [transition:backdrop-filter_0.2s,transform_1s_0.15s] motion-reduce:delay-0 w-1/6 mdlg:w-auto", !isVisible && "mdlg:-translate-y-[150%]", !sideBarOpen && !isVisible && "-translate-y-[150%]")}>
+                <span className="hidden mdlg:inline-block">About me</span>
+                <span
+                    className={clsx(sideBarOpen && "after:!rotate-0 before:!rotate-180", "inline-block align-baseline relative w-10 h-10 before:content-[''] before:block before:bg-grey before:absolute before:top-2/4 before:left-0 before:w-full before:h-1 after:content-[''] after:block after:bg-grey after:absolute after:top-2/4 after:left-0 after:duration-700 before:duration-700 motion-reduce:after:duration-0 motion-reduce:before:duration-0 after:w-full after:h-1 before:-translate-y-1/2 after:-translate-y-1/2 after:rotate-90 p-5 mx-10")}
+                />
+            </button>
+            <CustomLink
+                className={clsx(button({ rounded: "tlbr" }), "items-center hidden mdlg:flex justify-center [transition:backdrop-filter_0.2s,transform_1s_0.3s] motion-reduce:delay-0 uppercase", !isVisible && "-translate-y-[150%]")}
+                href={`mailto:${email}`}
+                target="_blank">
+                {email}
+                <Image
+                    src="/svgs/arrow-right.svg"
+                    alt="arrow right"
+                    width={18}
+                    height={11}
+                    className="inline ml-5 align-baseline"/>
+            </CustomLink>
+        </header>
     )
 }
